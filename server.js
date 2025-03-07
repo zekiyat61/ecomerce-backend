33eed31196df26df68eff4a5f502d71cc799a637
zekiyat <zekiyatatsh2009@gmail.com>
2025-03-07 12:05:27 +0300

    add remote

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3001",
       "https://zekiyaecomerce.netlify.app",
+      "https://zekiyatecommerce-admin.netlify.app",
     ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
